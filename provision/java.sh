1dad1c502ca05427772a5c928244683327d1aeb1
Fabricio Lemos <fabricio.lemos@gmail.com>
2016-09-05 22:25:47 -0400

    install java

---
+++ provision/java.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Installing from https://launchpad.net/~webupd8team/+archive/ubuntu/java
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+apt-get update
+apt-get -y install oracle-java8-installer
